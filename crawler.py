e418e4e96ed4ed2a0aae579db6a1ac762564bc73
CalvinY8 <46547265+CalvinY8@users.noreply.github.com>
2022-07-06 15:46:41 -0700

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -11,11 +11,6 @@ import lxml
 
 import urllib
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 import re
 
 from time import sleep, perf_counter
@@ -93,12 +88,6 @@ def create_csv():
     #sample_df means sample dataframe
     sample_df = pd.DataFrame(columns = columns)
 
-    #===selenium part===
-    # fire_fox_options = webdriver.FirefoxOptions()
-    # #fire_fox_options.headless = True #---comment out this line to see UI
-    # driver = webdriver.Firefox(options = fire_fox_options)
-    # driver.get(url)
-
     #---retrieve the data
     #the outer for loop increments the pages, giving each page to the inner for loop
 
@@ -115,6 +104,8 @@ def create_csv():
         #print(url_page) see that url incremented correctly
 
         soup = getSoupFromURL(url_page) #get the code containing jobs from the rest of the html on that page.
+
+        #WARN add some error check that prevents scraping non-existing pages(indeed doesn't have these pages)
 
         resultsCol = soup.find("td", {"id": "resultsCol"}) #we want only the resultsCol td
         jobsList = resultsCol.find(name="ul", attrs={"class":"jobsearch-ResultsList"})
@@ -192,47 +183,6 @@ def create_csv():
             job_post.append(job_url)
 
 
-            #===using selenium===
-
-            #---get data from iframe
-            #open the new page directly
-            # use selenium to access content in the iframe
-
-            #get the <a> of class jcs-JobTitle
-            value1 = id_a['data-jk']
-            new_page_url = url + "&vjk=" + value1
-            #print(new_page_url)
-
-            # driver.get(new_page_url)
-
-            # wait = WebDriverWait(driver, 25)
-
-            # #close annoying popup if it appears
-            # if driver.find_elements(By.CSS_SELECTOR, "button.popover-x-button-close.icl-CloseButton"):
-            #     #element = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "popover-x-button-close icl-CloseButton")))
-            #     btn = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "button.popover-x-button-close.icl-CloseButton")))
-            #     btn.click()
-
-            # #try to access i-frame
-            # wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, "vjs-container-iframe")))
-
-            # #for some reason this fails sometimes, despite increasing the wait time from 10 to 15
-            # if driver.find_elements(By.ID, "jobDescriptionText"):
-            #     desc_element = driver.find_element_by_id('jobDescriptionText')
-            #     #print(desc_element.text)
-            #     job_post.append(desc_element.text)
-            #     #print("length = " + str(len(desc_element.text)))
-            #     #csv files have a limit of 32,767 characters per cell.
-            #     #no problem, cause description length is like 3000-8000 chars
-
-            # else:
-            #     #print('n') #unable to get it. this should be visible on the csv. file.
-            #     job_post.append('--')
-            
-            # driver.switch_to.default_content()
-
-            #===selenium end===
-
             #YOU ARE HERE
             #using beautifulsoup instead of selenium. faster?
 
@@ -281,8 +231,6 @@ def create_csv():
 
     print('new csv data loaded')
 
-    #===for selenium===
-    #driver.close()
 
 def main():
 
@@ -302,30 +250,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-# using selenium to work on a dynamically generated webpage
-
-# ->where is the element being clicked?
-
-# iframe id="vjs-container-iframe"
-# title="Details of the selected job"
-
-# ->what triggers that iframe to appear?
-# presumably, clicking on element with class="cardOutline tapItem"
-
-#https://stackoverflow.com/questions/68419150/python-selenium-how-can-i-get-access-to-this-part-of-the-website
-
-#according to this, you can upgrade your existing bs4 script and don't need to rewrite it
-#https://medium.com/ymedialabs-innovation/web-scraping-using-beautiful-soup-and-selenium-for-dynamic-page-2f8ad15efe25
-
-#the inefficent way is just to use html parser on each job card's href
-#https://stackoverflow.com/questions/67504953/how-to-get-full-job-descriptions-from-indeed-using-python-and-beautifulsoup
-
-#selenium firefox driver headless?
-
-# selenium click on element
-# and then get the description.
 
 
 #the main objective is to analyze job description data
